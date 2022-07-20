59b7d8967d33737a173380a30ffd306558b4c075
dhudra67 <107625653+dhudra67@users.noreply.github.com>
2022-07-20 12:33:14 +0500

    Update guessing-game.py

--- guessing-game.py
+++ guessing-game.py
@@ -4,7 +4,7 @@ def guess(x):
     random_number = random.randint(1,x)
     guess = 0
     while guess != random_number:
-        guess = (input("Guess a random number: "))
+        guess = input("Guess a random number: ")
         if guess > random_number:
             print("your guess is too high")
         elif guess < random_number:
